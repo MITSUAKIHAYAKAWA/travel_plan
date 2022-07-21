8241cf7d07c61ff194fe3c4381d0cbda13aa825d
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-21 12:11:02 +0900

    deviseの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_033604) do
+ActiveRecord::Schema.define(version: 2022_07_21_025206) do
 
-  create_table "travels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "travels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "travel_time_one_id"
     t.integer "travel_time_two_id"
     t.integer "destination_id"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2022_07_20_033604) do
     t.string "travel_title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "nickname"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
