77271b8042d109b106a6428f6e0a0e70b8be868d
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-22 16:07:53 +0900

    ボタンを押すとpagetopに戻る機能を実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
